edc3caab034398b930aa69f91275d1254daaae2c
NataliaValchuk <trewax321@gmail.com>
2023-04-20 20:59:48 +0300

    udded librery, API key

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+// axios.get([перший аргумент], [другий аргумент]);
+
+//     {
+//       params: {
+//         key: '35609739-281e68efedb9f0ad474261401',
+//         q: searchKeyWord,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     }
+
+const fetchImage = async searchKeyWord => {
+  try {
+    const requestConfig = {
+      params: {
+        key: '35609739-281e68efedb9f0ad474261401',
+        q: searchKeyWord,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    };
+
+    const response = await axios.get('https://pixabay.com/api', requestConfig);
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchImage();
