f41e811f049955d74640e754a3cf0db529ee5cdd
NataliaValchuk <trewax321@gmail.com>
2023-04-23 18:11:11 +0300

    fixed tasks, udded styles css

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const formEl = document.querySelector('#search-form');
+const formEl = document.querySelector('.search-form');
 const loadMoreBtnEl = document.querySelector('.load-more');
 const galleryEl = document.querySelector('.gallery');
 const lightbox = new SimpleLightbox('.gallery a');
 
 let pageNumber = 1;
+
+const showLoadMoreBtn = () => {
+  loadMoreBtnEl.classList.remove('hidden');
+};
+
+const hideLoadMoreBtn = () => {
+  loadMoreBtnEl.classList.add('hidden');
+};
 
 const fetchImage = async (searchKeyWord, page) => {
   try {
@@ -28,48 +36,83 @@ const fetchImage = async (searchKeyWord, page) => {
     return response.data;
   } catch (error) {
     console.log(error);
-    return null;
   }
 };
 
 const renderImages = images => {
-  const photoCardsMarkup = images.map(({ webformatURL, likes, views, comments, downloads, largeImageURL }) => {
-    return `
-      <a href="${largeImageURL}" class="gallery__item">
-        <img src="${webformatURL}" alt="" loading="lazy" class="gallery__image" />
+  const photoCardsMarkup = images.map(
+    ({ webformatURL, likes, views, comments, downloads, largeImageURL }) => {
+      return `
+      <a href="${largeImageURL}" class="photo-card">
+        <img src="${webformatURL}" alt="" loading="lazy" class="photo-card__image" />
         <div class="info">
-          <p class="info-item"><b>Likes</b>${likes}</p>
-          <p class="info-item"><b>Views</b>${views}</p>
-          <p class="info-item"><b>Comments</b>${comments}</p>
-          <p class="info-item"><b>Downloads</b>${downloads}</p>
+          <p class="info__item"><b>Likes</b>${likes}</p>
+          <p class="info__item"><b>Views</b>${views}</p>
+          <p class="info__item"><b>Comments</b>${comments}</p>
+          <p class="info__item"><b>Downloads</b>${downloads}</p>
         </div>
       </a>
     `;
-  });
+    }
+  );
 
   galleryEl.insertAdjacentHTML('beforeend', photoCardsMarkup.join(''));
   lightbox.refresh();
 };
 
+const showErrorNotification = () =>
+  Notiflix.Notify.failure(
+    'Oops, something went wrong! Please try again later.'
+  );
+
+const handleNoImages = () => {
+  hideLoadMoreBtn();
+
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+};
+
+const clearGallery = () => {
+  galleryEl.innerHTML = '';
+};
+
+const increasePageNumber = () => {
+  pageNumber += 1;
+};
+
 const handleFormSubmit = async event => {
   event.preventDefault();
+
   const formData = new FormData(event.currentTarget);
   const searchQuery = formData.get('searchQuery').trim();
 
   if (!searchQuery) {
     Notiflix.Notify.warning('Please enter a search keyword!');
+
     return;
   }
 
   try {
     const { hits, totalHits } = await fetchImage(searchQuery, pageNumber);
-    galleryEl.innerHTML = '';
+
+    clearGallery();
     renderImages(hits);
+
+    console.log(hits.length, hits);
+
+    if (!hits.length) {
+      handleNoImages();
+
+      return;
+    }
+
     Notiflix.Notify.success(`Found ${totalHits} images`);
-    pageNumber += 1;
-    loadMoreBtnEl.disabled = false;
+
+    showLoadMoreBtn();
+    increasePageNumber();
   } catch (error) {
-    Notiflix.Notify.failure('Oops, something went wrong! Please try again later.');
+    showErrorNotification();
   }
 };
 
@@ -79,10 +122,11 @@ const handleLoadMoreBtnClick = async () => {
 
   try {
     const { hits } = await fetchImage(searchQuery, pageNumber);
+
     renderImages(hits);
-    pageNumber += 1;
+    increasePageNumber();
   } catch (error) {
-    Notiflix.Notify.failure('Oops, something went wrong! Please try again later.');
+    showErrorNotification();
   }
 };
 
@@ -90,18 +134,3 @@ formEl.addEventListener('submit', handleFormSubmit);
 loadMoreBtnEl.addEventListener('click', handleLoadMoreBtnClick);
 
 
-// // Вибираємо всі картки зображень та обгортаємо їх у посилання
-// const imageLinks = document.querySelectorAll('.image-card a');
-
-// // Створюємо групу карток зображень та додаємо її до бібліотеки
-// const imageGroup = [];
-// imageLinks.forEach(link => {
-//   imageGroup.push(link);
-// });
-// const lightbox = new SimpleLightbox(imageGroup);
-
-// // Викликаємо метод refresh() щоразу після додавання нової групи карток зображень
-// lightbox.refresh();
-
-
-console.log(formEl);
